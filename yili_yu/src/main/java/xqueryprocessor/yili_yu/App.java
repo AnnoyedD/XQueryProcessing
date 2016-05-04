8519baa7524ffec3810ba8cc2b4d91d0580d8cf3
Yili Ding <Yili Ding>
2016-05-04 14:23:55 -0700

    update

---
+++ yili_yu/src/main/java/xqueryprocessor/yili_yu/App.java
@@ -0,0 +1,13 @@
+package xqueryprocessor.yili_yu;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
